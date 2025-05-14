40adaac0a7d8e345717484bfe85ce11d10653c8f
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2025-05-14 18:38:23 +0530

    Add files via upload

---
+++ SQL/Project 1/SQL_Farmers_Insurance_Analysis_Data_Import_by_Aman_Shreevalli.sql
@@ -0,0 +1,71 @@
+
+-- You can only load data from local storage if the local_infile parameter is set to 1
+SET GLOBAL local_infile=1;
+-- You might need to change the local_infile parameter by editing it in the configuration file
+-- Open "my.ini" (Windows) or "my.cnf" (Mac/Linux) and set local_infile=1
+
+-- Another way to load a table is to create the schema first and then use the table data import wizard in MySQL Workbench
+CREATE SCHEMA IF NOT EXISTS ndap;
+
+USE ndap;
+
+CREATE TABLE IF NOT EXISTS FarmersInsuranceData (
+    rowID INT PRIMARY KEY,
+    srcYear INT,
+    srcStateName VARCHAR(255),
+    srcDistrictName VARCHAR(255),
+    InsuranceUnits INT,
+    TotalFarmersCovered INT,
+    ApplicationsLoaneeFarmers INT,
+    ApplicationsNonLoaneeFarmers INT,
+    InsuredLandArea FLOAT,
+    FarmersPremiumAmount FLOAT,
+    StatePremiumAmount FLOAT,
+    GOVPremiumAmount FLOAT,
+    GrossPremiumAmountToBePaid FLOAT,
+    SumInsured FLOAT,
+    PercentageMaleFarmersCovered FLOAT,
+    PercentageFemaleFarmersCovered FLOAT,
+    PercentageOthersCovered FLOAT,
+    PercentageSCFarmersCovered FLOAT,
+    PercentageSTFarmersCovered FLOAT,
+    PercentageOBCFarmersCovered FLOAT,
+    PercentageGeneralFarmersCovered FLOAT,
+    PercentageMarginalFarmers FLOAT,
+    PercentageSmallFarmers FLOAT,
+    PercentageOtherFarmers FLOAT,
+    YearCode INT,
+    Year_ VARCHAR(255),
+    Country VARCHAR(255),
+    StateCode INT,
+    DistrictCode INT,
+    TotalPopulation INT,
+    TotalPopulationUrban INT,
+    TotalPopulationRural INT,
+    TotalPopulationMale INT,
+    TotalPopulationMaleUrban INT,
+    TotalPopulationMaleRural INT,
+    TotalPopulationFemale INT,
+    TotalPopulationFemaleUrban INT,
+    TotalPopulationFemaleRural INT,
+    NumberOfHouseholds INT,
+    NumberOfHouseholdsUrban INT,
+    NumberOfHouseholdsRural INT,
+    LandAreaUrban FLOAT,
+    LandAreaRural FLOAT,
+    LandArea FLOAT
+);
+
+SHOW VARIABLES LIKE 'local_infile';
+-- used to display the current values of various server system variables
+-- These variables control the behavior and configuration of the MySQL server, covering aspects like storage, security, 
+-- networking, query optimization, etc.
+-- It tells the server to only display the system variables whose names match the pattern 'local_infile'
+
+LOAD DATA LOCAL INFILE 'C:/UPGRADE/Data Science/C2_Data_Toolkit/SQL_Graded_Group_Assessment/SQL_Assg_PMFBY_Dataset_Starter/Data_PMFBY/data.csv'
+INTO TABLE FarmersInsuranceData
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'          
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
+
